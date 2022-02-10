f594c2698486b6ee4aac2c55b3f5b2b0c5f89da1
gerssivaldosantos <canalandows@gmail.com>
2022-02-10 15:39:45 -0300

    fix: adding the parameters that miss

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ import SharedButton from './SharedButton';
 
 const sharedButton = new SharedButton("https://www.youtube.com/channel/UClvtUd7IO1L9BrCEuGkGRXw");
 
-sharedButton.bind(".btn-twitter");
-sharedButton.bind(".btn-facebook");
-sharedButton.bind(".btn-linkedin");
+sharedButton.bind(".btn-twitter", "twitter");
+sharedButton.bind(".btn-facebook", "facebook");
+sharedButton.bind(".btn-linkedin", "linkedin");
